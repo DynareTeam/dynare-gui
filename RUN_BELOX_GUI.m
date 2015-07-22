5be1e7f7290c2b1bb4995664b4481b24b088098e
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2015-07-22 11:12:25 +0200

    Revert "Initial commit (Imported codes from Milica)."
    This reverts commit 8696c0c6aceb282f3140ee5f0fc8bb124841b746.

--- RUN_BELOX_GUI.m
+++
@@ -1,6 +0,0 @@
-path(path,'Belox_GUI');
-path(path,['Belox_GUI' filesep 'GUILayout-v1p14']);
-path(path,['Belox_GUI' filesep 'GUILayout-v1p14' filesep '+uiextras']);
-path(path,['Belox_GUI' filesep 'GUILayout-v1p14' filesep 'Patch']);
-
-Belox_GUI_simulator
